3531d08b355f7b71ca319e69c8399fcaeb34ef3d
mdmahafuzur47 <mdmahafuzur4747@gmail.com>
2024-09-22 10:05:27 +0600

    create 6 files

---
+++ frontend/src/pages/home/StatisticsHome.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Container from '../../components/ui/Container'
+
+export default function StatisticsHome() {
+  return (
+    <div className=''>
+      <Container>
+        <div>
+          <img src="/images/home/c1.png" alt="img" />
+        </div>
+      </Container>
+    </div>
+  )
+}
